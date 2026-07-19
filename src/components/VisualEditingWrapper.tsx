0109ac6e6adc0f7302154927802a1d08bc317525
Gautam Jha <gautamjha.mailbox@gmail.com>
2026-07-19 22:53:12 +0530

    fix: import VisualEditing from client-component subpath to avoid Server Actions in static export

--- src/components/VisualEditingWrapper.tsx
+++ src/components/VisualEditingWrapper.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+"use client";
+
 import { useIsPresentationTool } from "next-sanity/hooks";
-import { VisualEditing } from "next-sanity/visual-editing";
+import { VisualEditing } from "next-sanity/visual-editing/client-component";
 
 export function VisualEditingWrapper() {
   const isPresentationTool = useIsPresentationTool();
