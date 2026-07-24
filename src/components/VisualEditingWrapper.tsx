103c2f6948f72d03b8bb48d3c0335e3b8e5f1629
Gautam Jha <gautamjha.mailbox@gmail.com>
2026-07-24 13:13:05 +0530

    Revert "chore: enable visual editing iframe context check gracefully"
    This reverts commit 13911abe37f47ec8d47f47f41fd492ac7bfb2b0f.

--- src/components/VisualEditingWrapper.tsx
+++ src/components/VisualEditingWrapper.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 
 const VisualEditing = dynamic(
   () => import("@sanity/visual-editing/react").then((mod) => ({ default: mod.VisualEditing })),
@@ -9,22 +8,5 @@ const VisualEditing = dynamic(
 );
 
 export function VisualEditingWrapper() {
-  const [inIframe, setInIframe] = useState(false);
-
-  useEffect(() => {
-    // Check if the current window is running inside an iframe
-    // and verify it is opened by Sanity Studio
-    if (
-      window.self !== window.top &&
-      (document.referrer.includes("sanity.studio") || document.referrer.includes("sanity.io"))
-    ) {
-      setInIframe(true);
-    }
-  }, []);
-
-  if (!inIframe) {
-    return null;
-  }
-
   return <VisualEditing portal />;
 }
